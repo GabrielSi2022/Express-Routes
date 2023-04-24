a31d1b7d884fa2ff6ad4571f39e8a6a380d866e1
Gabriel Henrique  Bueno <gabrielhenriquesi1996@gmail.com>
2023-04-24 14:40:15 -0300

    Atts do curso de Javascript

--- Express/server.js
+++ Express/server.js
@@ -1,22 +1,68 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.CONNECTIONSTRING)
+  .then(() => {
+    app.emit("conectado");
+  })
+  .catch((e) => console.log("falha na conexao com o banco de dados!"));
+
+// const session = require("express-session");
+// const MongoStore = require("connect-mongo");
+// const flash = require("connect-flash");
+
+const session = require("express-session"),
+  MongoStore = require("connect-mongo"),
+  flash = require("connect-flash");
+
 const routes = require("./routes");
 const path = require("path");
-const { middlewareGlobal } = require("./src/middlewares/middleware");
+const helmet = require("helmet");
+const csrf = require("csurf");
+const {
+  middlewareGlobal,
+  checkCsrfError,
+  csrfMiddleware,
+} = require("./src/middlewares/middleware");
 
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "public")));
+
+const sessionOptions = session({
+  secret:
+    "sdfkasdfasdfjksdhfkjsdfkjhsadfkas 123hsdjfkhskd qwe sdfskdfj qweqw jweqqw qwejqwe sdfklasdlk",
+  store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true,
+  },
+});
+
+app.use(sessionOptions);
+app.use(flash());
 
 app.set("views", path.resolve(__dirname, "src", "views"));
 app.set("view engine", "ejs");
 
+app.use(csrf());
+
 //Nossos proprios middlewares
 app.use(middlewareGlobal);
-
+app.use(checkCsrfError);
+app.use(csrfMiddleware);
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log("Acessar http://localhost:3000");
-  console.log("Servidor executando na porta 3000");
+app.on("conectado", () => {
+  app.listen(3000, () => {
+    console.log("Acessar http://localhost:3000");
+    console.log("Servidor executando na porta 3000");
+  });
 });
